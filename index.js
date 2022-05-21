b2c5b5bde1bf9fb4be99b6248627c74213fb3a6a
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-21 23:04:55 +0200

    Temporary

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/documentation', (req, res) => {
 
 
 // Read
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) =>{
+app.get('/movies', (req, res) =>{
   Movies.find()
   .then((movies) => {
       res.status(201).json(movies);
