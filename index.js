6d069b167fe19a3479cf653f17009257bb715bc1
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-26 23:32:21 +0200

    new index

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ let auth = require('./auth')(app);
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movime-api.herokuapp.com/'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movime-api.herokuapp.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
       return callback(new Error(message ), false);
     }
     return callback(null, true);
