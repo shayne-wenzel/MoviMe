436d029eefac9465f82e1f6582593675b35d8090
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-28 01:49:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('/documentation', (req, res) => {
 
 
 // Read
-app.get('/movies', (req, res) =>{
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) =>{
   Movies.find()
   .then((movies) => {
       res.status(201).json(movies);
