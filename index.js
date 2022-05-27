f0aada5ea96d9ed7a4fd68b416a16dc8712fb429
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-27 20:15:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let auth = require('./auth')(app);
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'http://testsite.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
