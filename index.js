22343f3d1f4b37dbc56b3857542914bc6bee22bc
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-27 23:06:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let auth = require('./auth')(app);
 
-const cors = require('cors');
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// const cors = require('cors');
+// app.use(cors());
 
 // const cors = require('cors');
 
