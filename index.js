5d526d9bdc7159203b003c4f23176ba054fd9f4c
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-27 20:21:54 +0200

    attempt 6

--- index.js
+++ index.js
@@ -23,19 +23,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let auth = require('./auth')(app);
 
 const cors = require('cors');
+app.use(cors());
 
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'http://testsite.com'];
+// const cors = require('cors');
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'http://testsite.com'];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // let xhr = new XMLHttpRequest();
 // xhr.open('GET', 'http://localhost:8080', 'http://localhost:1234', 'https://movime-api.herokuapp.com', true);
