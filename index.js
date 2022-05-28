82cba9137c55ee11f47acf94c1763b230ae2db53
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-28 19:51:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.post('/users',
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
-    ], passport.authenticate('jwt', { session: false }), (req, res) =>
+    ], (req, res) =>
     {
     let errors = validationResult(req);
 
